20091aab5a7ad7a397c869a90b9b9d33367e3813
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-01-22 15:46:05 +0200

    hw

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const elems = {
+    breedSelect: document.querySelector('.breed-select'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+    catInfo: document.querySelector('.cat-info')
+};
+console.log(elems)
